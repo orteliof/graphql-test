d1d810925c09f2443240bce34706fa2542df87ad
Ing. Ortelio F. Hernandez  Socarras <orteliof.hdezs@gmail.com>
2019-12-09 18:00:26 -0500

    add mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
+const mongoose = require('mongoose');
 const schema = require('./schemas/schema');
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const cors = require('cors');
+
+// Connect to database
+const mongoUri = 'mongodb://localhost:27017/graphql-test';
+mongoose.connect(
+    mongoUri, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then((db) => console.log(`Connected to database at [${mongoUri}]`))
+    .catch((err) => console.error(`Error: ${err}`));
 
 // Creating ApolloServer instance.
 const server = new ApolloServer({ schema });
